9fae24c2a0569494329293a87a165147ebced300
thilak459 <thilakawm123@gmail.com>
2026-03-03 15:40:12 +0530

    Creating arithmetic operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,14 @@
+//Arithmetic operators
+console.log("Arithmetic Operators");
+const a=2,b=3;
+let c=0;
+console.log("a+b",c=a+b);
+console.log("a-b",c=a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a%b",a%b); //Modulus
+console.log("a**b",a**b); //Exponential
+console.log("a++",a++); //post-increment
+console.log("++a",++a); //pre-increment
+console.log("a--",a--); //post-decrement
+console.log("--a",--a); //pre-decrement
