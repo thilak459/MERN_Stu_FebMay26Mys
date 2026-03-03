6db4765add125ddc22572aafa502b72d27abd0fb
thilak459 <thilakawm123@gmail.com>
2026-03-03 15:41:43 +0530

    Adding logical operators

---
+++ W3/D2/P3.js
@@ -0,0 +1,6 @@
+//Logical operators
+console.log("Logical operators");
+let a=true,b=false;
+console.log("a&&b=",a&&b); //logical AND
+console.log("a||b=",a||b); //logical OR
+console.log("!a",!a); //logical NOT
