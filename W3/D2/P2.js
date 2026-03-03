1be67be3306c7e6b7dcf7b762da7aa198422aaef
thilak459 <thilakawm123@gmail.com>
2026-03-03 15:40:54 +0530

    Adding comparison operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,12 @@
+//Comparison operators
+console.log("Comparison operators");
+let x=3,y='3';
+console.log("x==y: ",x==y); //Loose equality
+console.log("x===y: ",x===y); //Strict equality
+console.log("x!=y: ",x!=y); //Loose inequality
+console.log("x!==y: ",x!==y); //Strict inequality
+console.log("x>3: ",x>3);
+console.log("x<3: ",x<3);
+console.log("x>=3: ",x>=3);
+console.log("x<=3: ",x<=3);
+
