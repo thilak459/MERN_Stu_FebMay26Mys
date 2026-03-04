6e8c54f47fd3b704e5d5ec083770ac8bd41d8fed
thilak459 <thilakawm123@gmail.com>
2026-03-04 14:25:20 +0530

    Adding while loop

---
+++ W3/D3/P2.js
@@ -0,0 +1,14 @@
+//while loop
+console.log("While loop");
+let count=0;
+while(count<5){
+    console.log("count:",count);
+    count++;
+}
+
+//do-while loop
+let value=0;
+do{
+    console.log("Value :",value);
+    value++;
+}while(value <3);
