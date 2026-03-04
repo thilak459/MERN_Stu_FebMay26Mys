5d7247a24b88e1e78992baf20183617b007236d1
thilak459 <thilakawm123@gmail.com>
2026-03-04 14:24:54 +0530

    Adding for loop

---
+++ W3/D3/P1.js
@@ -0,0 +1,15 @@
+// for loop
+console.log("for loop examples");
+for(let i=0;i<5;i++){ //forward loop
+    console.log("i",i); 
+}
+
+for(let i=5;i>0;i--){ //Backward loop
+    console.log("i=",i); 
+}
+//Nested for loop
+for(let i=1;i<=3;i++){
+    for(let j=1;j<=2;j++){
+        console.log(`i=${i},j=${j}`);
+    }
+}
