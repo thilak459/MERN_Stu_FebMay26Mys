9047ead1ef47e69516aff6e3b82d403fae7fcf5b
thilak459 <thilakawm123@gmail.com>
2026-03-04 14:25:53 +0530

    Adding break and continue in for loop

---
+++ W3/D3/P3.js
@@ -0,0 +1,15 @@
+//break
+for(let i=0;i<5;i++){ //forward loop
+    if(i===2){
+        console.log("i",i); 
+        break;
+    }
+}
+
+//continue
+for(let i=0;i<5;i++){ //forward loop
+    if(i===2){
+        console.log("i",i); 
+        continue;
+    }
+}
