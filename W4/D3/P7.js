6e50e2197fa6122efa3ecf9724cbf846946f2f13
thilak459 <thilakawm123@gmail.com>
2026-03-11 16:35:47 +0530

    response and status

---
+++ W4/D3/P7.js
@@ -0,0 +1,27 @@
+const existingPostBtn = document.getElementById("existingPostBtn");
+const missingPostBtn = document.getElementById("missingPostBtn");
+const statusOutput = document.getElementById("statusOutput");
+
+function fetchPost(url){
+    statusOutput.textContent = "Fetching from "+url;
+    fetch(url)
+    .then(function(response){
+        if(!response.ok){
+            throw new Error("Http error status code: "+response.status);
+        }
+        return response.json();
+    })
+    .then(function(data){
+        statusOutput.textContent = "Success: "+JSON.stringify(data,null,2);
+    })
+    .catch(function(error){
+        statusOutput.textContent = "Error: "+error.message;
+    });
+}
+existingPostBtn.addEventListener("click",function(){
+    fetchPost("https://jsonplaceholder.typicode.com/posts/10");
+});
+
+missingPostBtn.addEventListener("click",function(){
+    fetchPost("https://jsonplaceholder.typicode.com/posts/101");
+});
