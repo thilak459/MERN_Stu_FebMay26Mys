cce5ca12e2b7dad52225568c2ddf960ed3a40587
thilak459 <thilakawm123@gmail.com>
2026-03-17 16:18:23 +0530

    Introduction to promises

---
+++ W5/D2/P8.js
@@ -0,0 +1,17 @@
+//Introduction to promises
+console.log("Program started.");
+function getWelcomeMessage(){
+    return new Promise(function(resolve){
+        setTimeout(()=>{
+            resolve("Welcome to Promises");
+        },1000);
+    });
+}
+const messagePromise = getWelcomeMessage();
+
+console.log("Promise created. Result not ready yet.");
+
+messagePromise.then(function(message){
+    console.log(message);
+});
+console.log("Program continues while the promise is pending.");
