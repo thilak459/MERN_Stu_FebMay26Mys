b9ef563281b678fdf79660102496e864d821f50c
thilak459 <thilakawm123@gmail.com>
2026-03-17 16:18:43 +0530

    Promise states: pending, fulfilled, rejected

---
+++ W5/D2/P9.js
@@ -0,0 +1,19 @@
+//Promise states: pending, fulfilled, rejected
+const fulfilledPromise = new Promise(function(resolve){
+    console.log("fullfilledPromise is pending");
+    setTimeout(function(){
+        resolve("fulfilledPromise is fulfilled.");
+    },1000);
+});
+const rejectedPromise = new Promise(function(resolve,reject){
+    console.log("rejectedPromise is pending.");
+    setTimeout(function(){
+        reject("rejectedPromise is rejected.");
+    },1500);
+});
+fulfilledPromise.then(function(message){
+    console.log(message);
+});
+rejectedPromise.catch(function(message){
+    console.log(message);
+});
