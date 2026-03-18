2696b0fc33afb6d2f4fc5ff22788fad82197d226
thilak459 <thilakawm123@gmail.com>
2026-03-18 10:40:47 +0530

    Handling errors with try/catch

---
+++ W5/D3/P3.js
@@ -0,0 +1,22 @@
+// Handling errors with try/catch
+function loadCustomerProfile(){
+    return new Promise(function(resolve,reject){
+        const isServiceAvailable = false;
+        if(isServiceAvailable){
+            resolve("Success! Customer profile loaded.");
+        }
+        else{
+            reject("Unsuccessfull! Customer profile unavailable.");
+        }
+    });
+}
+async function showCustomerProfile(){
+    try{
+        const message = await loadCustomerProfile();
+        console.log(message);
+    }
+    catch(error){
+        console.error("Error:",error);
+    } 
+}
+showCustomerProfile();
