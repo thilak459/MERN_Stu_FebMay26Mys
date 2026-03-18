db9f1cacbcd01d9737133b64dfe067870d08e0e7
thilak459 <thilakawm123@gmail.com>
2026-03-18 11:25:00 +0530

    Combining multiple promise-based steps with async/await

---
+++ W5/D3/P4.js
@@ -0,0 +1,28 @@
+// Combining multiple promise-based steps with async/await
+function getOrderId(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(502);
+        },500);
+    });
+}
+function getOrderDetails(orderId){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve({
+                id:orderId,
+                product: "Laptop",
+                quantity: 2
+            });
+        },700);
+    });
+}
+async function showOrderSummary(){
+    const orderId = await getOrderId();
+    console.log("Order Id:",orderId);
+
+    const orderDetails = await getOrderDetails(orderId);
+    console.log("Product:",orderDetails.product);
+    console.log("Quantity:",orderDetails.quantity);
+}
+showOrderSummary();
