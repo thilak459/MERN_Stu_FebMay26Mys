5f09732324e0f423ea794214058634038e10d959
thilak459 <thilakawm123@gmail.com>
2026-03-25 11:55:06 +0530

    Directory(folder) operations: mkdir, readdir and rmdir/rm

---
+++ W6/D3/P6.js
@@ -0,0 +1,19 @@
+//Directory(folder) operations: mkdir, readdir and rmdir/rm
+const fs = require("fs");
+const path = require("path");
+
+const demoDir = path.join(__dirname,"demo-folder");
+
+//to create folder
+fs.mkdirSync(demoDir,{recursive:true});
+
+fs.writeFileSync(path.join(demoDir,"File1.txt"),"File1 content");
+fs.writeFileSync(path.join(demoDir,"File2.txt"),"File2 content");
+
+//To check folder contents
+const entries = fs.readdirSync(demoDir);
+console.log("Directory contents:",entries);
+
+//delete folder
+fs.rmSync(demoDir,{recursive:true, force:true});
+console.log("Directory exists?",fs.existsSync(demoDir));
