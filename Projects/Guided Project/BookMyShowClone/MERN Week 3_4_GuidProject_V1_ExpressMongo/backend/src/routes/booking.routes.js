6eaef05453c4af902e6947c948e8553e11b07af9
thilak459 <thilakawm123@gmail.com>
2026-04-22 10:11:29 +0530

    Created the movie booking route with auth middleware

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/booking.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const {protect} = require("../middleware/auth.middleware");
+
+// User booking route
+router.post("/",protect,(req,res)=>{
+    res.send("Create booking");
+});
