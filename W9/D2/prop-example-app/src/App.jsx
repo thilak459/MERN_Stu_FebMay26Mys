6cee5ebb188db6d0108fd2347e46ed0482672a2c
thilak459 <thilakawm123@gmail.com>
2026-04-28 15:41:10 +0530

    Added Props destructuring

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { PropBasics } from './components/P1'
+import { PropDestructuring } from './components/P2';
 
 function App() {
  return(
   <>
-    <PropBasics/>
+    {/* <PropBasics/> */}
+    <PropDestructuring/>
   </>
  );
 }
