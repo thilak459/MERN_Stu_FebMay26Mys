88487be2327e50eb5b0b894ff26c88412adf0b2a
thilak459 <thilakawm123@gmail.com>
2026-04-28 15:27:21 +0530

    Props basics structure and styling

---
+++ W9/D2/prop-example-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import { PropBasics } from './components/P1'
+
+function App() {
+ return(
+  <>
+    <PropBasics/>
+  </>
+ );
+}
+
+export default App
