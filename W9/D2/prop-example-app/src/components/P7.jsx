93369efad08ab2b2e9dab9e85489922fb73adf6e
thilak459 <thilakawm123@gmail.com>
2026-04-29 12:42:35 +0530

    Passing objects and arrays as props

---
+++ W9/D2/prop-example-app/src/components/P7.jsx
@@ -0,0 +1,32 @@
+// Passing objects and arrays as props
+function UserCard({user}){
+    return(
+        <div className="card">
+            <p>Name: {user.name}</p>
+            <p>Role: {user.role}</p>
+        </div>
+    );
+}
+function SkillsList({skills}){
+    return(
+        <ul>
+            {skills.map((skill)=>(
+                <li key={skill}>{skill}</li>
+            ))}
+        </ul>
+    )
+}
+export function ObjectsAndArrayProps(){
+    const user = {
+        name: 'Thilak',
+        role: 'Developer'
+    };
+    const skills = ['React','TailwindCSS','Vite'];
+    return(
+        <>
+            <h2>Passing Objects and Arrays as Props</h2>
+            <UserCard user={user} />
+            <SkillsList skills={skills} />
+        </>
+    );
+}
