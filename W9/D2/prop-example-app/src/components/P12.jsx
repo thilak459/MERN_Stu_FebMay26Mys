f89e49bc2906e8728e0f5c4c87ef3ee75a2f8b6b
thilak459 <thilakawm123@gmail.com>
2026-04-29 15:13:51 +0530

    // Higher order components(HOC)

---
+++ W9/D2/prop-example-app/src/components/P12.jsx
@@ -0,0 +1,23 @@
+// Higher order components(HOC) example
+const Greeting = ({name}) => {
+    return <h1>Hello, {name}</h1>
+}
+// Higher order components(HOC)
+const WithBorder = (OriginalComponent) =>{
+    return function EnhancedComponent(props){
+        return(
+            <div className="setBorder">
+                <OriginalComponent{...props}/>
+            </div>
+        );
+    }
+}
+const GreetingWithBorder = WithBorder(Greeting);
+
+export function HOCExample(){
+    return(
+        <>
+            <GreetingWithBorder name="Thilak"/>
+        </>
+    );
+}
