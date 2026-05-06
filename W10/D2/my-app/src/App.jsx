e37d43c389e92c0c640a3db85fa04957867c394c
thilak459 <thilakawm123@gmail.com>
2026-05-06 12:53:42 +0530

    useRef hook

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import { UseContextIntro } from './components/P1'
 import { CustomHooksIntro } from './components/P2'
+import { UserRefIntro } from './components/P3'
 
 function App() {
   return (
     <>
-      <UseContextIntro/>
+      {/* <UseContextIntro/> */}
       {/* <CustomHooksIntro/> */}
+      <UserRefIntro/>
     </>
   )
 }
