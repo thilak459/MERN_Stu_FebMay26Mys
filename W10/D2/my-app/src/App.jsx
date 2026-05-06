c235626f99d0f2bcdd1a0b63ad3face216ff4222
thilak459 <thilakawm123@gmail.com>
2026-05-06 13:38:11 +0530

    useReducer hook

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import { UseContextIntro } from './components/P1'
 import { CustomHooksIntro } from './components/P2'
 import { UserRefIntro } from './components/P3'
+import { UseReducerIntro } from './components/P4'
 
 function App() {
   return (
     <>
       {/* <UseContextIntro/> */}
       {/* <CustomHooksIntro/> */}
-      <UserRefIntro/>
+      {/* <UserRefIntro/> */}
+      <UseReducerIntro/>
     </>
   )
 }
