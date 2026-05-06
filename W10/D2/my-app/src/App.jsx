252953ad1b2a389a1d26f67eb158da327fe32085
thilak459 <thilakawm123@gmail.com>
2026-05-06 12:18:19 +0530

    Used Custom hooks to add title

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { UseContextIntro } from './components/P1'
+import { CustomHooksIntro } from './components/P2'
 
 function App() {
   return (
     <>
       <UseContextIntro/>
+      {/* <CustomHooksIntro/> */}
     </>
   )
 }
