1435b90d4b83855689c1b381fc04704aef8beba6
thilak459 <thilakawm123@gmail.com>
2026-05-07 15:41:16 +0530

    Stop event propagation

--- W10/D3/my-app/src/App.jsx
+++ W10/D3/my-app/src/App.jsx
@@ -6,6 +6,7 @@ import { UseCallbackEvents } from './components/P5'
 import { EventDeligation } from './components/P6'
 import { FormsPattern } from './components/P7'
 import { KeyboardEvent } from './components/P8'
+import { EventObj } from './components/P9'
 
 function App() {
 
@@ -17,7 +18,8 @@ function App() {
       {/* <UseCallbackEvents /> */}
       {/* <EventDeligation/> */}
       {/* <FormsPattern/> */}
-      <KeyboardEvent/>
+      {/* <KeyboardEvent/> */}
+      <EventObj/>
     </>
   )
 }
---
+++ W10/D3/my-app/src/components/P9.jsx
@@ -0,0 +1,34 @@
+// Stop event propagation
+export function EventObj(){
+    const handleParentClick = (event) => {
+        alert(`Parent clicked target: ${event.target.tagName} currentTarget: ${event.currentTarget.tagName}`);
+    };
+    const handleChildClick = (event) => {
+        event.stopPropagation();
+        alert(`Child clicked (propagation stopped) target: ${event.target.tagName} currentTarget: ${event.currentTarget.tagName}`);
+    };
+
+    // Form submit handler
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("Form submitted safely by reventing default refresh");
+    };
+
+    const handleBoxClick = (event) => {
+        alert(`Box clicked target: ${event.target.tagName} currentTarget: ${event.currentTarget.tagName}`)
+    };
+
+    return(
+        <section onClick={handleParentClick} style={{padding:'20px',backgroundColor:'yellow'}}>
+            <h2>Event Object</h2>
+            <button onClick={handleChildClick}>Child Button</button>
+            <button onClick={handleSubmit}>Submit Form</button>
+
+            <div onClick={handleBoxClick} style={{padding:'20px', backgroundColor:'red'}}>
+                <span style={{padding:'20px', backgroundColor:'blue', display:'inline-block'}}>
+                    Inner Span
+                </span>
+            </div>
+        </section>
+    )
+}
