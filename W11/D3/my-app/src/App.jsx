ea6c9952764074b1fa8a0a8532f16a909962799e
thilak459 <thilakawm123@gmail.com>
2026-05-13 14:47:34 +0530

    useEffect Lifecycle

---
+++ W11/D3/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {ManagingApiState} from './components/P1'
+import { UseEffectLifecycle } from './components/P2'
+
+function App() {
+
+  return (
+    <>
+      {/* <ManagingApiState/> */}
+      <UseEffectLifecycle/>
+    </>
+  )
+}
+
+export default App
