b42edddf9a9249b962fa3bfe2fdfb21629705572
thilak459 <thilakawm123@gmail.com>
2026-05-14 12:19:48 +0530

    Error boundary & file upload progress

--- W11/D3/my-app/src/App.jsx
+++ W11/D3/my-app/src/App.jsx
@@ -4,6 +4,7 @@ import { UseEffectLifecycle } from './components/P2'
 import { AxiosLifeCycle } from './components/P3'
 import { CRUDOperation } from './components/P4'
 import { PaginationCaching } from './components/P5'
+import { UploadErrorBoundary } from './components/P6'
 
 function App() {
 
@@ -13,7 +14,8 @@ function App() {
       {/* <UseEffectLifecycle/> */}
       {/* <AxiosLifeCycle/> */}
       {/* <CRUDOperation/> */}
-      <PaginationCaching/>
+      {/* <PaginationCaching/> */}
+      <UploadErrorBoundary/>
     </>
   )
 }
