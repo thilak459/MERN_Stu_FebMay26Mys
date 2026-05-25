af9d576e56f5cc3f142e826936d3fa9e6f5ce042
thilak459 <thilakawm123@gmail.com>
2026-05-25 12:38:24 +0530

    Validation & error messages

--- W11/D4/my-app/src/App.jsx
+++ W11/D4/my-app/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { ControlledForm } from './components/P1'
+import { ValidationForm } from './components/P2'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <ControlledForm/>
+      {/* <ControlledForm/> */}
+      <ValidationForm/>
     </>
   )
 }
