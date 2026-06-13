b7a1fc108d0be63d1fbee857289e3c87de93afdb
thilak459 <thilakawm123@gmail.com>
2026-06-13 12:02:20 +0530

    BrowserRouter and its related functions

---
+++ W12/Final Frontend Project/src/components/DashboardSection.jsx
@@ -0,0 +1,55 @@
+import { NavLink, Outlet } from "react-router-dom";
+export default function DashboardSection(){
+    return(
+        <section style={styles.container}>
+            <aside style={styles.sidebar}>
+                <h3>Dashboard</h3>
+                <nav style={styles.sidebarLinks}>
+                    <NavLink to="/dashboard" end style={getLinkStyle}>
+                        Overview
+                    </NavLink>
+                    <NavLink to="/dashboard/movies" end style={getLinkStyle}>
+                        Movies
+                    </NavLink>
+                    <NavLink to="/dashboard/shows" end style={getLinkStyle}>
+                        Shows
+                    </NavLink>
+                </nav>
+            </aside>
+            <main style={styles.content}>
+                {/* Outlet acts as a placeholder */}
+                <Outlet />
+            </main>
+        </section>
+    );
+}
+
+function getLinkStyle({isActive}){
+    return{
+        textDecoration: "none",
+        color: isActive? "#d32f2f" : "#333",
+        fontWeight: isActive ? "bold" : "normal",
+        // borderBottom: isActive ? "2px solid #d32f2f" : NamedNodeMap,
+        // paddingBottom: "4px"
+    };
+}
+const styles = {
+    container: {
+        display:"flex",
+        gap: "20px",
+        marginTop: "20px"
+    },
+    sidebar: {
+        minWidth: "180px",
+        borderRight: "2px solid #ddd",
+        paddingRight: "20px"
+    },
+    sidebarLinks: {
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px"
+    },
+    content: {
+        flex:1
+    }
+};
